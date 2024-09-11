568d53639d33d96d35e8dad2da75a30b839c3410
ashik13en <ashikurrahman13en@gmail.com>
2024-09-12 04:55:48 +0600

    added js practise code

---
+++ app.js
@@ -0,0 +1,65 @@
+
+
+
+// function sayHello(name){
+//     console.log(name)
+// }
+
+// sayHello("Fuad")
+
+
+// function sum(a,b){
+//     return a - b;
+// }
+
+// let result = sum(23,47)
+// console.log(result)
+
+
+// scope 
+// global scope
+
+// let video = "playlist";
+
+// function displayVar() {
+//     console.log(video); // Can access playlist
+// }
+
+
+// let payment = 'paid';
+
+// if(payment === "paid"){
+//     displayVar()
+// }else{
+//     console.log('please pay first')
+// }
+
+
+// local  scope 
+
+// function myFunction() {
+//     let localVar = "I am local";
+//     console.log(localVar); 
+// }
+// myFunction()
+// console.log(localVar); 
+
+
+
+// home work
+
+// 1. Create a function that takes two numbers and returns their Multiplication.
+// 2. Write a function that prints "Good Morning!" if it's morning (use a parameter), otherwise prints "Good Evening!".
+// 3. Create a simple calculator that asks the user to input two numbers. Then, use arithmetic operators to calculate and display the following: Sum ,Difference, Product,Quotient,Remainder.
+// 4. Write a program that prompts the user to input their age and checks if the user is eligible to vote. Use comparison operators to compare the age to 18.
+// 5.  If the score is greater than or equal to 50, display "You passed!", otherwise display "You failed!" Use the ternary operator for this
+
+
+// extra homework
+// function ki?
+// scope ki?
+// js expressions ki?
+// js operators ki?
+
+
+
